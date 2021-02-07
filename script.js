776778edc07b619c68a9cf04164e8d71e5d6d70c
dazzlingeffe <irusya9@mail.ru>
2021-02-07 19:40:40 +0300

    gallery slider done

--- script.js
+++ script.js
@@ -1,3 +1,38 @@
 let openPic = function(e) {
+	let gallerySection = document.getElementById('gallery');
+	gallerySection.id = 'container';
 	
+	document.querySelector('main').style.paddingTop = '70px';
+	
+	gallerySection.insertAdjacentHTML('beforeEnd', '<div class="image-nav"><ul><li><button id="prev">previous</button></li><li><button id="next">next</button></li></ul></div>');
+	
+	document.querySelectorAll('.gallery-item').forEach(item => {item.className = 'image-view'});
+	e.target.className += ' active';
+
+	let prevBtn = document.getElementById('prev');
+	let nextBtn = document.getElementById('next');
+	let imgCount = document.querySelectorAll('.image-view').length;
+
+	prevBtn.addEventListener('click', function() {
+		let currentImg = document.querySelector('.active');
+		let currentId = currentImg.id;
+		currentImg.classList.remove('active');
+		if (currentId === '1') {
+			document.getElementById(imgCount).classList.add('active');
+		} else {
+			document.getElementById(parseInt(currentId, 10) - 1).classList.add('active');
+		}
+	});
+	nextBtn.addEventListener('click', function() {
+		let currentImg = document.querySelector('.active');
+		let currentId = currentImg.id;
+		currentImg.classList.remove('active');
+		if (currentId == imgCount) {
+			document.getElementById('1').classList.add('active');
+		} else {
+			document.getElementById(parseInt(currentId, 10) + 1).classList.add('active');
+		}
+	});
 }
+
+document.querySelectorAll('.gallery-item').forEach(item => { item.addEventListener('click', openPic)});
